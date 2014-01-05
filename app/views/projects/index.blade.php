16510cb8ad1bfb1facffb93dfa3226c6ad302ecd
Scott <ssessner@tipoki.(none)>
2014-01-05 11:51:01 -0600

    added project index

--- app/views/projects/index.blade.php
+++ app/views/projects/index.blade.php
@@ -1,1 +1,13 @@
-index.blade
+@extends('layout')
+
+@section('content')
+
+@foreach($projects as $project)
+	<ul>
+		<h1>{{$project->name}}</h1>
+		@foreach($projects->cameras() as $camera)
+			<h2>{{$camera->id}}</h2>
+		@endforeach
+	</ul>
+@endforeach
+@stop
